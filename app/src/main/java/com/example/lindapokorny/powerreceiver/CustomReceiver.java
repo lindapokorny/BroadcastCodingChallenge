02fb63861bead0ce2854bd47f8a1993eabf608a2
Linda Pokorny <lindapokrny@pursuit.org>
2018-12-16 17:10:17 -0500

    CodingChallengeAnswer

---
+++ app/src/main/java/com/example/lindapokorny/powerreceiver/CustomReceiver.java
@@ -0,0 +1,37 @@
+package com.example.lindapokorny.powerreceiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+import static android.content.Intent.ACTION_HEADSET_PLUG;
+
+public class CustomReceiver extends BroadcastReceiver {
+
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String intentAction = intent.getAction();
+        if (intentAction != null) {
+            String toastMessage = "unknown intent action";
+            switch (intentAction) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    toastMessage = "Power connected!";
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    toastMessage = "Power disconnected!";
+                    break;
+                case ACTION_CUSTOM_BROADCAST:
+                    toastMessage = "Custom Broadcast Received";
+                    break;
+                case Intent.ACTION_HEADSET_PLUG:
+                    toastMessage = "You are plugged in!";
+                    break;
+            }
+            Toast.makeText(context, toastMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
---
+++ app/src/main/java/com/example/lindapokorny/powerreceiver/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.lindapokorny.powerreceiver;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private CustomReceiver mReceiver = new CustomReceiver();
+    private static final String ACTION_CUSTOM_BROADCAST =
+            BuildConfig.APPLICATION_ID + ".ACTION_CUSTOM_BROADCAST";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        this.registerReceiver(mReceiver, filter);
+        LocalBroadcastManager.getInstance(this).registerReceiver(mReceiver, new IntentFilter(ACTION_CUSTOM_BROADCAST));
+
+
+        IntentFilter filter1 = new IntentFilter();
+        filter1.addAction(Intent.ACTION_HEADSET_PLUG);
+    }
+
+
+
+    @Override
+    protected void onDestroy() {
+        this.unregisterReceiver(mReceiver);
+        super.onDestroy();
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+
+        this.unregisterReceiver(mReceiver);
+        LocalBroadcastManager.getInstance(this).unregisterReceiver(mReceiver);
+    }
+
+    public void sendCustomBroadcast(View view) {
+        Intent customBroadcastIntent = new Intent(ACTION_CUSTOM_BROADCAST);
+        LocalBroadcastManager.getInstance(this).sendBroadcast(customBroadcastIntent);
+    }
+}
+
